3415cf8e755a9d485dc2b8fe6767ac37f7df434d
DiRaiks <t.fane@mail.ru>
2025-02-07 18:44:19 +0300

    feat: refactor contract method calls with generic utility functions

---
+++ utils/call-contract.ts
@@ -0,0 +1,54 @@
+import { printError } from "@utils";
+import { getAccount } from "@providers";
+import { getChain } from "@configs";
+
+type Writeable<T> = { -readonly [P in keyof T]: T[P] };
+
+type GetFirst<T extends unknown[]> = T extends [infer First, infer _Second]
+  ? First
+  : T extends any
+  ? []
+  : T;
+
+export const callWriteMethod = async <
+  T extends { write: Record<string, (...args: any[]) => Promise<any>> },
+  M extends keyof T["write"] & string
+>(
+  contract: T,
+  methodName: M,
+  payload: Writeable<GetFirst<Parameters<T["write"][M]>>> | never[],
+  value?: bigint
+) => {
+  try {
+    const method = contract.write[methodName];
+    const tx = await method?.(payload, {
+      account: getAccount(),
+      chain: getChain(),
+      value,
+    });
+
+    console.table({ Transaction: tx });
+  } catch (err) {
+    printError(err, `Error when calling write method ${methodName}`);
+  }
+};
+
+export const callReadMethod = async <
+  T extends { read: Record<string, (...args: any[]) => Promise<any>> },
+  M extends keyof T["read"] & string
+>(
+  contract: T,
+  methodName: M,
+  ...payload: Parameters<T["read"][M]>
+) => {
+  try {
+    const method = contract.read[methodName];
+    const result = await method?.(payload);
+    // TODO: do message better or show in called place
+    console.table({ Result: result });
+
+    return result;
+  } catch (err) {
+    printError(err, `Error when calling read method ${methodName}`);
+  }
+};
--- utils/index.ts
+++ utils/index.ts
@@ -3,3 +3,4 @@ export * from "./data-validators";
 export * from "./resolve-path";
 export * from "./sleep";
 export * from "./error-handler";
+export * from "./call-contract";
