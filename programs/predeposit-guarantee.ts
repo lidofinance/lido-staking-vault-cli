3de34a07519a5b2d72f54731ad1220a399f4b273
DiRaiks <t.fane@mail.ru>
2025-03-04 17:13:50 +0300

    feat: add create-proof-and-check command for PDG proof verification

--- programs/predeposit-guarantee.ts
+++ programs/predeposit-guarantee.ts
@@ -1,10 +1,43 @@
 import { program } from 'command';
+import { getCLProofVerifierContract } from 'contracts';
 
 import { createPDGProof, getFirstValidatorGIndex } from 'utils/index.js';
 
 const predepositGuarantee = program
   .command('pdg')
   .description('predeposit guarantee contract');
+
+predepositGuarantee
+  .command('create-proof-and-check')
+  .description(
+    'create predeposit proof by validator index and check by test contract',
+  )
+  .argument('<validator-index>', 'validator index')
+  .action(async (validatorIndex: bigint) => {
+    const clProofVerifierContract = getCLProofVerifierContract();
+
+    const packageProof = await createPDGProof(Number(validatorIndex));
+    const { proof, pubkey, childBlockTimestamp, withdrawalCredentials } =
+      packageProof;
+
+    await clProofVerifierContract.read.TEST_validatePubKeyWCProof([
+      { proof, pubkey, validatorIndex, childBlockTimestamp },
+      withdrawalCredentials,
+    ]);
+
+    console.info('-----------------proof verified-----------------');
+    console.info('------------------------------------------------');
+    console.info('----------------------proof----------------------');
+    console.info(proof);
+    console.info('---------------------pubkey---------------------');
+    console.table(pubkey);
+    console.info('---------------childBlockTimestamp---------------');
+    console.table(childBlockTimestamp);
+    console.info('--------------withdrawalCredentials--------------');
+    console.table(withdrawalCredentials);
+    console.info('------------------------------------------------');
+    console.info('-----------------------end-----------------------');
+  });
 
 predepositGuarantee
   .command('create-proof')
