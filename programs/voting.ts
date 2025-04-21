ad38af196d55d829c5954e54d1c8949f5bc57dd3
DiRaiks <t.fane@mail.ru>
2025-04-21 17:41:27 +0300

    feat: refactor voting CLI and introduce dashboard commands for minting and burning shares

--- programs/voting.ts
+++ programs/voting.ts
@@ -1,14 +1,8 @@
-import { Address } from 'viem';
 import { Option } from 'commander';
 import { program } from 'command';
-import { getVaultHubContract, getVotingContract } from 'contracts';
+import { getVotingContract } from 'contracts';
 import { voteLastVoting } from 'features';
-import {
-  callWriteMethodWithReceipt,
-  callReadMethod,
-  logInfo,
-  getCommandsJson,
-} from 'utils';
+import { callReadMethod, logInfo, getCommandsJson } from 'utils';
 
 const voting = program.command('vote').description('voting contract');
 voting.addOption(new Option('-cmd2json'));
@@ -16,41 +10,6 @@ voting.on('option:-cmd2json', function () {
   logInfo(getCommandsJson(voting));
   process.exit();
 });
-
-voting
-  .command('connect-vault')
-  .description('vote and connect vault to the hub')
-  .description('connects a vault to the hub (vault master role needed)')
-  .argument('<address>', 'vault address')
-  .argument(
-    '<shareLimit>',
-    'maximum number of stETH shares that can be minted by the vault',
-  )
-  .argument('<reserveRatio>', 'minimum Reserve ratio in basis points')
-  .argument(
-    '<reserveRatioThreshold>',
-    'reserve ratio that makes possible to force rebalance on the vault (in basis points)',
-  )
-  .argument('<treasuryFeeBP>', 'treasury fee in basis points')
-  .action(
-    async (
-      address: Address,
-      shareLimit: bigint,
-      reserveRatio: bigint,
-      reserveRatioThreshold: bigint,
-      treasuryFeeBP: bigint,
-    ) => {
-      const contract = await getVaultHubContract();
-
-      await callWriteMethodWithReceipt(contract, 'connectVault', [
-        address,
-        shareLimit,
-        reserveRatio,
-        reserveRatioThreshold,
-        treasuryFeeBP,
-      ]);
-    },
-  );
 
 voting.command('get-lv').action(async () => {
   const { contract } = getVotingContract();
