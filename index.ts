82a22d6a4ba20ab028369edeffc8ba83ece6a640
DiRaiks <t.fane@mail.ru>
2025-04-24 22:28:48 +0300

    feat: export utility functions from utils module for improved accessibility

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import { program } from './command/index.js';
 import { logError } from './utils/logging/console.js';
 import './programs/index.js';
 
+export * from './utils/index.js';
+
 program.parseAsync(process.argv).catch((error) => {
   logError('CLI Error:', error);
   process.exit(1);
