502e3a7e7e2c4b6c035c44906b9781d9b0ebf96d
DiRaiks <t.fane@mail.ru>
2025-06-15 23:16:59 +0300

    feat: add testnet warning message to CLI for user awareness

--- index.ts
+++ index.ts
@@ -6,7 +6,24 @@ import './programs/index.js';
 
 export * from './utils/index.js';
 
-program.parseAsync(process.argv).catch((error) => {
-  logError('CLI Error:', error.message);
-  process.exit(1);
-});
+const showTestnetWarning = () => {
+  console.info(
+    '\n⚠️  Heads-up! All Testnet data will be erased on 23 Jun 2025 as a part of the Web UI migration to the stVaults Testnet-2 contracts. Back up anything important.',
+  );
+  console.info(
+    '🔄 Testnet-1 CLI functionality will remain available in the testnet-1 branch.',
+  );
+  console.info(
+    '📄 Contracts info: https://docs.lido.fi/deployed-contracts/hoodi-lidov3/\n',
+  );
+};
+
+program
+  .parseAsync(process.argv)
+  .then(() => {
+    showTestnetWarning();
+  })
+  .catch((error) => {
+    logError('CLI Error:', error.message);
+    process.exit(1);
+  });
