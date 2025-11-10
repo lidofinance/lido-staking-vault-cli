17e9bd9a7d479b42f5f3d725c392c77094e0b6cf
DiRaiks <t.fane@mail.ru>
2025-11-10 16:41:10 +0400

    refactor: improve vault creation logic by adding confirm expiry and node operator fee rate retrieval

--- programs/contracts/vault-factory/write.ts
+++ programs/contracts/vault-factory/write.ts
@@ -2,7 +2,12 @@ import { Address } from 'viem';
 import { program } from 'command';
 import { Option } from 'commander';
 
-import { createVault, prepareCreateVaultPayload } from 'features';
+import {
+  createVault,
+  getNodeOperatorFeeRate,
+  getConfirmExpiry,
+  prepareCreateVaultPayload,
+} from 'features';
 import { RoleAssignment } from 'types';
 import {
   confirmCreateVaultParams,
@@ -56,12 +61,16 @@ vaultFactoryWrite
       quantity: string,
       options: { roles: RoleAssignment[] },
     ) => {
+      const confirmExpiryValue = await getConfirmExpiry(confirmExpiry);
+      const nodeOperatorFeeRateValue =
+        await getNodeOperatorFeeRate(nodeOperatorFeeRate);
+
       const createVaultData = prepareCreateVaultPayload({
         defaultAdmin,
         nodeOperator,
         nodeOperatorManager,
-        confirmExpiry,
-        nodeOperatorFeeRate,
+        confirmExpiry: confirmExpiryValue,
+        nodeOperatorFeeRate: nodeOperatorFeeRateValue,
         quantity,
         roles: options.roles,
       });
@@ -136,12 +145,16 @@ vaultFactoryWrite
       quantity: string,
       options: { roles: RoleAssignment[] },
     ) => {
+      const confirmExpiryValue = await getConfirmExpiry(confirmExpiry);
+      const nodeOperatorFeeRateValue =
+        await getNodeOperatorFeeRate(nodeOperatorFeeRate);
+
       const createVaultData = prepareCreateVaultPayload({
         defaultAdmin,
         nodeOperator,
         nodeOperatorManager,
-        confirmExpiry,
-        nodeOperatorFeeRate,
+        confirmExpiry: confirmExpiryValue,
+        nodeOperatorFeeRate: nodeOperatorFeeRateValue,
         quantity,
         roles: options.roles,
       });
