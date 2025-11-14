a51dd7835f2116d0d5350ae667b7a3c4991a1bfb
DiRaiks <t.fane@mail.ru>
2025-11-14 13:09:04 +0300

    refactor: remove isContract function from VaultViewer ABI and update VaultViewer address

--- contracts/vault-viewer.ts
+++ contracts/vault-viewer.ts
@@ -5,7 +5,7 @@ import { VaultViewerAbi } from 'abi';
 import { getChain, getElUrl } from 'configs';
 
 const VaultViewerAddresses: Record<number, Address> = {
-  [hoodi.id]: '0xBa9a3127f053a6548FE3874326569E1821783e03',
+  [hoodi.id]: '0x510b4CE9CdA8E5C9268D242a51356fF9Dc2bd73b',
 };
 
 export const getVaultViewerContract = () => {
