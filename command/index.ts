830c9ee42a809d0c65eba1743d94a65ec13e755c
Evgeny Taktarov <je.mouth@gmail.com>
2026-02-19 16:14:49 +0100

    chore: add help text

--- command/index.ts
+++ command/index.ts
@@ -20,3 +20,15 @@ program.option(
 program.option('--wallet-connect', 'use wallet connect to send transactions');
 program.option('--csv <file>', 'write table output to CSV file');
 program.option('--json', 'output result in JSON format');
+
+program.addHelpText('afterAll', () => {
+  return `
+Common options:
+  --populate-tx          output raw transaction data instead of sending
+  -y, --yes              automatically confirm prompts and proceed without asking
+  --no-cache-use         do not use cache for fetching data from IPFS
+  --wallet-connect       use wallet connect to send transactions
+  --csv <file>           write table output to CSV file
+  --json                 output result in JSON format
+  `;
+});
