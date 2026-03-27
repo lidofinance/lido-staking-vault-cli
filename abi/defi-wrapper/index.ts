70aad48d2350acc410cb0ccfcdf4ec74b5b5667d
Evgeny Taktarov <je.mouth@gmail.com>
2026-03-27 13:23:12 +0100

    chore: naming

--- abi/defi-wrapper/index.ts
+++ abi/defi-wrapper/index.ts
@@ -1,6 +1,6 @@
 export * from './Distributor.js';
 export * from './StvPool.js';
-export * from './stv-st-eth-pool.js';
+export * from './StvStEthPool.js';
 export * from './WithdrawalQueue.js';
 export * from './Factory.js';
 export * from './TimeLock.js';
